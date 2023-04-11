a339a5888f2f2e28bbedffcc3c9676fbcdad4e60
Yury Stepanov <yury.a.stepanov@gmail.com>
2023-04-11 15:42:11 +0300

    YC deploy

---
+++ pre-launch-operations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /code/ctracker || exit
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
