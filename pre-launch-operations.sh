6686fd95a049e93ac6f36a35d6b94016f243dd20
Yury Stepanov <yury.a.stepanov@gmail.com>
2023-04-11 17:01:04 +0300

    YC deploy

--- pre-launch-operations.sh
+++ pre-launch-operations.sh
@@ -9,3 +9,7 @@ python manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
+
+#Run server
+echo "Running uwsgi server"
+uwsgi --ini /code/config/uwsgi/uwsgi.ini
